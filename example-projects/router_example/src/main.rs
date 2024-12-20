42e39548ad0b04916e014fc91a48bcfa1fc42e04
wolf88844 <wolf88840@163.com>
2024-12-20 14:23:02 +0800

    feat(router_example): 添加 Dogge 页面并集成 reqwest
    - 在路由中添加了新的 Dogge 页面
    - 使用 reqwest 库从 API 获取随机狗图片
    - 优化了导航栏，增加了 Dogge 页面的链接
    - 在项目中集成了 serde 和 tokio 依赖

--- example-projects/router_example/src/main.rs
+++ example-projects/router_example/src/main.rs
@@ -14,7 +14,11 @@ fn main() {
 fn App() -> Element {
     rsx! {
         head {
-            link { rel: "icon", r#type: "image/x-icon", href: asset!("/assets/favicon.ico") }
+            link {
+                rel: "icon",
+                r#type: "image/x-icon",
+                href: asset!("/assets/favicon.ico"),
+            }
         }
         document::Link { rel: "stylesheet", href: asset!("/assets/router.css") }
         Router::<Route> {}
@@ -41,6 +45,8 @@ enum Route{
         #[redirect("/",|| Route::BlogList{})]
         #[redirect("/:name",|name:String| Route::BlogPost{name})]
     #[end_nest]
+    #[route("/dogge")]
+    Dogge {},
     #[route("/:..route")]
     PageNotFound { route:Vec<String> }
 
@@ -85,7 +91,9 @@ fn NavBar()->Element{
                 li {
                     Link { to: Route::BlogList {}, "My Blog" }
                 }
-            
+                li {
+                    Link { to: Route::Dogge {}, "Dogge" }
+                }
             }
         }
         Outlet::<Route> {}
@@ -132,4 +140,42 @@ fn BlogList()->Element{
             }
         }
     }
+}
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ApiResponse {
+    message: String,
+    status: String,
+}
+
+#[component]
+fn Dogge() -> Element {
+    let mut future = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<ApiResponse>()
+            .await
+    });
+    match &*future.read_unchecked(){
+        Some(Ok(response))=>rsx!{
+            button { onclick: move |_| future.restart(), "Click to fetch another dog" }
+            div {
+                img {
+                    max_width: "500px",
+                    max_height: "500px",
+                    src: "{response.message}",
+                }
+            }
+        },
+        Some(Err(_))=>rsx!{
+            div { "Failed to fetch dog" }
+        },
+        None=>rsx!{
+            div { "Loading..." }
+        }
+    }
+    
 }
